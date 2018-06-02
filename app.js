db165bccf3b22c3939ef2106e10eee5d984d65cb
Karolina Siwkowska <trallala00@gmail.com>
2018-06-02 17:03:51 +0200

    Set uo views

--- app.js
+++ app.js
@@ -13,6 +13,14 @@ var app = express();
 }
 app.use(logger);*/
 
+//11.view Engine
+app.set('view engine', 'ejs');
+//12.specify what folder we want to use
+app.set('views', path.join(__dirname, 'views'));
+
+
+
+
 
 //7.Body parser Middleware
 app.use(bodyParser.json());
@@ -20,16 +28,42 @@ app.use(bodyParser.urlencoded({
     extended: false
 }));
 
-//put static resources into folder,set static path
+//8.put static resources into folder,set static path
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-//.5 set up route for "/" using get request.
+/*//9.to parse j.son
+var people = [{
+        name: 'Jeff',
+        age: '20'
+},
+    {
+        name: 'Bill',
+        age: '30'
+},
+    {
+        name: 'John',
+        age: '40'
+},
+    {
+        name: 'Sarah',
+        age: '50'
+}
+             ];
+
+
+//10.instead send can use json array object
 app.get("/", function (req, res) {
-    res.send('Hello You');
+    res.json(people);
+});*/
+
+/*//.5 set up route for "/" using get request.
+app.get('/', function (req, res) {
+    res.send('Hello');
+});*/
+app.get('/', function (req, res) {
+    res.render('index');
 });
-
-
 //4.we need to listen our app on port
 app.listen(3000, function () {
     console.log('Server started on port 3000');
